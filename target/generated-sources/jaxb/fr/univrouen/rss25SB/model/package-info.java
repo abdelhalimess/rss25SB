06744b870c1e3427b88020f36c152b21b400eec7
abdelhalimess <esselami.abdelhalim@gmail.com>
2025-05-27 04:16:00 +0200

    Finalisation de Projet

--- target/generated-sources/jaxb/fr/univrouen/rss25SB/model/package-info.java
+++ target/generated-sources/jaxb/fr/univrouen/rss25SB/model/package-info.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par Eclipse Implementation of JAXB, v3.0.0 
 // Voir https://eclipse-ee4j.github.io/jaxb-ri 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2025.05.26 à 10:42:53 PM CEST 
+// Généré le : 2025.05.27 à 02:50:33 AM CEST 
 //
 
 @jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/2005/Atom", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
